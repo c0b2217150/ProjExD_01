9f3938b76025d1edf6d46b9852f1bb6f160764f2
c0b22171 <c0b2217150@edu.teu.ac.jp>
2023-11-21 16:48:45 +0900

    練習2

--- pygame_image.py
+++ pygame_image.py
@@ -6,16 +6,19 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")  # 練習１：背景画像Surfaceの生成
+    kk_img = pg.image.load("ex01/fig/3.png")  # 練習２：こうかとん画像Surfaceの生成
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習２：こうかとんの左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [0, 0])  # 練習４：背景画像の表示
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(10)
 
 
 if __name__ == "__main__":
